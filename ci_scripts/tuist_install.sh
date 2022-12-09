efce8c0350bbff38cb49d3fd38e3d11f3c95a8c2
Siwon Kim <siwonkim@zuminternet.com>
2022-12-09 10:37:55 +0900

    refactor: script 수정

---
+++ ci_scripts/tuist_install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "tuist install..."
+curl -Ls https://install.tuist.io | bash
+
+echo "tuist generate..."
+tuist clean --path ..
+tuist fetch --path ..
+tuist generate --path ..
+
+echo "😢"
